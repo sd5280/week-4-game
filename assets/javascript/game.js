8d6d36566eb3d66fe3efe55dec60616fc4e34d7a
sd5280 <sandi5280fordu@gmail.com>
2017-12-03 17:30:14 -0700

    changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// Generate and display random number to match between 19-120 (randomN)
+//Generate and associate randomGN with individual gems. randomGN should differ between gems.
+
+//Need gem images to have a function when clicked and associated randomly generated number (randomGN).
+//Gems random number between 1-12
+
+//Gem random number displays in your total score div, summation from each gem click of randomGN.
+
+//Total score > randomN, add 1 loss to losses
+//Total score == randomN, add 1 win to wins
+
+// Refreshing page starts everything over, no retention of prior games/counts.
